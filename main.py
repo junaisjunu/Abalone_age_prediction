05cc415d9ac2b72cfe7f89a2a6f117dfacbb0b28
junais <junaisk456@gmail.com>
2024-04-29 23:06:42 +0530

    added data transformation component

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.pipeline.stage_01_data_ingestion_pipeline import DataIngestionPipeline
+from src.pipeline.stage_02_data_transformation_pipeline import DataTransformationPipeline
 from src import logger
 from src.excption.exception import customexception
 import sys
@@ -11,5 +12,16 @@ if __name__ == "__main__":
         obj = DataIngestionPipeline()
         obj.main()
         logger.info(f"{stage_name} completed successfully!")
+    except Exception as e:
+        raise customexception(e, sys)
+
+
+stage_name = "Data Transformation"
+if __name__ == "__main__":
+    try:
+        logger.info("stage - {stage_name} has started ")
+        obj = DataTransformationPipeline()
+        obj.main()
+        logger.info(f"stage {stage_name} completed successfully!")
     except Exception as e:
         raise customexception(e, sys)
